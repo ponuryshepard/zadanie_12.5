57cd71ff83a8654dff15c279421a7bd2042d2de8
patryk <perseusz91@gmail.com>
2017-05-22 20:40:17 +0200

    nowecytaty

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,14 @@
 var tweetLink = "https://twitter.com/intent/tweet?text=";
+var prefix = "https://cors-anywhere.herokuapp.com/";
 var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
 
 function getQuote() {
-    $.getJSON(quoteUrl, createTweet);
+    $.getJSON(prefix + quoteUrl, createTweet);
+    $.ajaxSetup({ 
+    	cache: false
+    });
 }
+
 
 function createTweet(input) {
     var data = input[0];
@@ -16,10 +21,6 @@ function createTweet(input) {
     }
 
     var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
-
-    if (tweetText.length > 140) {
-    getQuote();
-    }
 
     if (tweetText.length > 140) {
     	getQuote();
